969c95f84427384adc7118abc181d3ce0290f798
Rohit Kadam <rohitkadam25635@gmail.com>
2025-03-31 11:21:53 +0530

    abhi wala

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,12 +14,7 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-if(process.env.NODE_ENV !== "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
-}
+
 
 app.use(express.json());
 app.use(cookieParser());
@@ -44,7 +39,12 @@ app.use((req, res, next) => {
 app.use("/api/auth", authRoutes); 
 app.use("/api/messages", messageRoutes); 
 
-
+if(process.env.NODE_ENV !== "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 server.listen(PORT, ()=> {
   console.log("server is running on PORT:" + PORT);
